40f21aefb2c68a01af9d346ef384be242c84b893
Tanmay Pal <tanmayspal2003@gmail.com>
2023-12-21 12:39:30 +0530

    Chai aur React | Video 02 Completed | Create react projects

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello Chai or React | Tanmay Pal</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Chai or React with Vite | Tanmay Pal</h1>
+  )
+}
+
+export default App
