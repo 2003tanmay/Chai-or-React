cd69a778d66f6190d326a64880fc7279912d09be
Tanmay Pal <tanmayspal2003@gmail.com>
2023-12-21 17:24:54 +0530

    Chai aur React | Video 04 Completed | Create my own react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./Chai"
 
 function App() {
+  const username = "Tanmay Pal"
 
   return (
     <>
-      <h1>Chai or React with Vite | Tanmay Pal</h1>
+      <h1>Chai or React with Vite | {username}</h1>
       <Chai />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherUser = "Sonam"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google ',
+  anotherUser
+)
+
+const anotherElement = (
+  <a href="https://google.com" target='_balnk'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <MyApp />
+  // anotherElement
+  // reactElement
+  <App />
 )
