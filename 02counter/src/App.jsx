0ccb4d59f5a0754b4fec3aecfa4796b0016fcab7
Tanmay Pal <tanmayspal2003@gmail.com>
2023-12-22 11:54:38 +0530

    Chai aur React | Video 08 Completed | State as a Snapshot and Queueing a series of State updates

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,6 +10,16 @@ function App() {
   const addValue = () => {
     if (counter != 20) {
       setCounter(counter + 1)
+      // setCounter(counter + 1) it all update the same counter 6 because react fiber take these
+      // setCounter(counter + 1) updation in a batches and useState send this in a batches and  
+      // setCounter(counter + 1) react re-render the page only when one event completed so in our
+      // setCounter(counter + 1) case onClick event re-render after all the setCounter() so in each
+      // setCounter(6 +1) these type of value goes and it update only counter as + 1.
+
+      setCounter((counter) => counter + 1) //when we like to update the same state variable multiple
+      setCounter(counter => counter + 1) //times before the next render, instead of passing the next
+      setCounter(counter => counter + 1) //state value like setCounter(counter+1) we can pass a next 
+      // state based on the previous one in the queue, like setCounter(counter => counter + 1).
     }
   }
 
