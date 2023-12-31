c1e851839ccefd557ca051f28a602bc06674bdb1
Tanmay Pal <tanmayspal2003@gmail.com>
2023-12-31 21:45:35 +0530

    Chai aur React | Video 24 Completed | Added form, Real time editor and slug values

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,6 +3,15 @@ import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./container/Container";
+import Login from "./Login";
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Signup from "./Signup";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
 
 export {
     Header,
@@ -10,4 +19,13 @@ export {
     Logo,
     Container,
     LogoutBtn,
+    Login,
+    AuthLayout,
+    Button,
+    Input,
+    PostCard,
+    Select,
+    Signup,
+    RTE,
+    PostForm,
 }
