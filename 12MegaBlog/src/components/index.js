bb9a95c56a267eaf6b3d466baa302a95f3d4fdf7
Tanmay Pal <tanmayspal2003@gmail.com>
2023-12-31 12:29:02 +0530

    Chai aur React | Video 22 Completed | Build Production grade react components | used forwerd ref

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,13 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from "./container/Container";
 
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn,
 }
